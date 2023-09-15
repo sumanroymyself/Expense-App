68245b4a255877c82bf2abb9ad52f504546c2305
sumanroy1995 <sumanroy8909@gmail.com>
2023-09-15 23:24:58 +0530

    button function defined

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+//total amount init
+let totalAmount= 0;
+
 //getting the ref for input amount element
-const input_amount= document.querySelector
+const input_amount= document.querySelector("#input-amnt");
 //getting the ref for total element
 const total_out= document.querySelector("#total-out");
 //getting the ref for the button element
@@ -7,7 +10,11 @@ const btn= document.querySelector("#add-button");
 //giving the click ability to the button element
 btn.addEventListener("click", clickEventHandler);
 
-
+//defining the click event
 function clickEventHandler(){
-    console.log("button working");
+    //console.log(input_amnt.value);
+    const strAmnt= input_amount.value;
+    const amount= parseInt(strAmnt);
+    totalAmount+= amount;
+    total_out.innerHTML= totalAmount;
 }
